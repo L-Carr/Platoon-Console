4dd14501c01bae9b5f60f11cd067763d8d677655
black-magk <landon.c.carr@gmail.com>
2024-04-22 13:29:43 -0700

    Initializing Project 1.2

--- frontend/platoon-console/src/App.jsx
+++ frontend/platoon-console/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   const [count, setCount] = useState(0)
 
--- frontend/platoon-console/src/main.jsx
+++ frontend/platoon-console/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.scss';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
