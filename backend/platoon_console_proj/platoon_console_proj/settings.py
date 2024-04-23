ac1c11c22bb145e6600566db16475d162dace1e8
black-magk <landon.c.carr@gmail.com>
2024-04-23 12:21:01 -0700

    DB Name Added to Settings.py

--- backend/platoon_console_proj/platoon_console_proj/settings.py
+++ backend/platoon_console_proj/platoon_console_proj/settings.py
@@ -75,10 +75,11 @@ WSGI_APPLICATION = 'platoon_console_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DATABASE_NAME"),
     }
 }
 
