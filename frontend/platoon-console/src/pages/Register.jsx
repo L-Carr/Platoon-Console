efa82315e8a55282cbfdf3a44e923541621b881a
dagaither <gaither@protonmail.com>
2024-04-24 15:46:53 -0400

    33-build-register-page

---
+++ frontend/platoon-console/src/pages/Register.jsx
@@ -0,0 +1,168 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Register = () => {
+
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const [password, setPassword] = useState("")
+    const [verifyPassword, setVerifyPassword] = useState("")
+    const [successMessage, setSuccessMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate()
+
+    const checkPassword = () => {
+        return password !== verifyPassword;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setSuccessMessage("");
+        setErrorMessage("");
+        try {
+            const userData = {
+                "first_name": firstName,
+                "last_name": lastName,
+                "email": email,
+                "phone_number": phoneNumber,
+                "password": password
+            };
+
+            let response = await axios.post("http://127.0.0.1:8000/", userData, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const { token, display_name } = response.data;
+
+            localStorage.setItem('token', token);
+            localStorage.setItem('display_name', display_name);
+
+            setErrorMessage("");
+            setSuccessMessage("Registration successful!");
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+            setPhoneNumber("");
+            setPassword("");
+            
+            setTimeout(() => {
+                navigate("/");
+            }, 2000);
+
+            console.log(response)
+
+        } catch (error) {
+            if (error.response) {
+                const errorMessage = error.response.data;
+                if (errorMessage && errorMessage.message === "User with this Email already exists.") {
+                    setErrorMessage(errorMessage.message);
+                } else {
+                    console.log('Error', errorMessage);
+                }
+            } else {
+                console.log('Error', error.message);
+            }
+        }
+    };
+
+    return (
+        <>
+            <div className="card-container" style={{ marginTop: "2rem", width: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <h1 className="mainH1">Register</h1>
+                <Form style={{ width: "300px", marginTop: "20px", marginBottom: "20px" }} onSubmit={handleSubmit}>
+                    <FormGroup>
+                        <Input
+                            id="exampleFirstName"
+                            name="firstName"
+                            placeholder="First Name"
+                            type="firstName"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+                    </FormGroup>
+                    {' '}
+                    <FormGroup>
+                        <Label
+                            for="exampleLastName"
+                            hidden
+                        >
+                            Last Name
+                        </Label>
+                        <Input
+                            id="exampleLastName"
+                            name="lastName"
+                            placeholder="Last Name"
+                            type="lastName"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+                    </FormGroup>
+                    {' '}
+                    <FormGroup>
+                        <Input
+                            id="exampleEmail"
+                            name="email"
+                            placeholder="Email"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </FormGroup>
+                    {' '}
+                    <FormGroup>
+                        <Input
+                            id="examplePhoneNumber"
+                            name="phoneNumber"
+                            placeholder="Phone Number"
+                            type="phoneNumber"
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNumber(e.target.value)}
+                        />
+                    </FormGroup>
+                    {' '}
+                    <FormGroup>
+                        <Input
+                            id="examplePassword"
+                            name="password"
+                            placeholder="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </FormGroup>
+                    {' '}
+                    <FormGroup>
+                        <Input
+                            id="exampleVerifyPassword"
+                            name="verifyPassword"
+                            placeholder="Verify Password"
+                            type="password"
+                            value={verifyPassword}
+                            onChange={(e) => setVerifyPassword(e.target.value)}
+                        />
+                    </FormGroup>
+                    {' '}
+                    <Button type="Submit">Submit</Button>
+                </Form>
+                {successMessage ? (
+                    <>
+                        <p style={{ color: 'green' }}>{successMessage}</p>
+                    </>
+                ) : (
+                    <>
+                        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+                        {checkPassword() && <p style={{ color: 'red' }}>Passwords do not match!</p>}
+                        <p>Already have an account? <Link tag="link" to="/login">Log in</Link></p>
+                    </>
+                )}
+            </div>
+        </>
+    )
+};
+
+export default Register;
--- frontend/platoon-console/src/router.jsx
+++ frontend/platoon-console/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom"
 import App from './App';
 import Homepage from './pages/Homepage'
 import Login from "./pages/Login"
+import Register from "./pages/Register";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
             {
                 path: "login/",
                 element: <Login />,
+            },
+            {
+                path: "register/",
+                element: <Register />,
             },
         ],
     }
