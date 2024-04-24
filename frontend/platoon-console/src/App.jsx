b118f7f1e53d830b238aeb1e11bad49370437320
Neka Blair <ourblairfamily@gmail.com>
2024-04-24 14:45:15 -0500

    add login usestate

--- frontend/platoon-console/src/App.jsx
+++ frontend/platoon-console/src/App.jsx
@@ -2,11 +2,28 @@ import { useState, useEffect } from 'react'
 import  { Outlet, useNavigate } from 'react-router-dom'
 import NavBar from './components/NavBar'
 
+const useLoggedIn = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const useLoggedIn = () => {
+    useEffect(() => {
+      //check for auth with token in local storage
+      const isAuthenticated = localStorage.getItem('token') !== null
+      setIsLoggedIn(isAuthenticated)
+    }, [])
+  
+    return isLoggedIn
+  }
+}
+
+
 function App() {
+  const isLoggedIn = useLoggedIn()
+  
 
   return (
     <>
-    <NavBar />
+    <NavBar isLoggedIn={isLoggedIn}  />
     <div className="mainContent" style={{paddingTop: "6rem"}}>
     <Outlet />
     </div>
