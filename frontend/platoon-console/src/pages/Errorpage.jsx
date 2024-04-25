a4f908a2da372a8ef6840db7c31beac6a70a9db9
Michael Roy <michaelr.social@mrcodewizard.com>
2024-04-24 21:05:23 -0400

    Build out Error Page

--- frontend/platoon-console/src/pages/Errorpage.jsx
+++ frontend/platoon-console/src/pages/Errorpage.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
+import lostImage from '../../public/lost.png';
 
-const Errorpage = () => {
+const ErrorPage = () => {
+  const containerStyles = {
+    position: 'relative',
+    textAlign: 'center',
+    color: 'white'
+  };
+
+  const imageStyles = {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover'
+  };
+
+  const overlayTextStyles = {
+    position: 'absolute',
+    top: '35%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    fontSize: '50px',
+    letterSpacing: '1px',
+    textShadow: '2px 2px 4px #000000'
+  };
+
+  const shadowStyles = {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)' // Adding a dark semi-transparent overlay
+  };
+
   return (
-    <div>Errorpage</div>
-  )
+    <div style={containerStyles}>
+      <h1>Error 404</h1>
+      <p>Are you lost?</p>
+      <div style={{ position: 'relative', overflow: 'hidden' }}>
+        <img src={lostImage} alt="Lost" style={imageStyles} />
+        <div style={shadowStyles}></div>
+        <div style={overlayTextStyles}>404_ YOU'RE BEYOND THE BORDERS.</div>
+      </div>
+    </div>
+  );
 }
 
-export default Errorpage
+export default ErrorPage;
