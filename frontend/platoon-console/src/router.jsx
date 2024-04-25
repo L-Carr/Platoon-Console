762eaabacfa34309db4dc65cbcf25b5f6f657a84
Dominic Colbert <donniemoe716@gmail.com>
2024-04-25 14:31:56 -0400

    Implemented Basic Grouping functionality

--- frontend/platoon-console/src/router.jsx
+++ frontend/platoon-console/src/router.jsx
@@ -1,30 +1,35 @@
-import { createBrowserRouter } from "react-router-dom"
-import App from './App';
-import Homepage from './pages/Homepage'
-import Login from "./pages/Login"
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ErrorPage from "./pages/Errorpage";
+import GroupProgramming from "./pages/GroupProgramming";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                index: true,
-                element: <Homepage />,
-            },
-            {
-                path: "login/",
-                element: <Login />,
-            },
-            {
-                path: "register/",
-                element: <Register />,
-            },
-        ],
-    },
-    {
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "login/",
+        element: <Login />,
+      },
+      {
+        path: "register/",
+        element: <Register />,
+      },
+      {
+        path: "groups/",
+        element: <GroupProgramming />,
+      },
+    ],
+  },
+  {
     path: "*",
     element: <ErrorPage />,
   },
