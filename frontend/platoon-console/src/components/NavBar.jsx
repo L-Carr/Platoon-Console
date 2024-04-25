2f10b16a9bdaa1ff2e51852d12e3e730d0470194
Neka Blair <ourblairfamily@gmail.com>
2024-04-25 10:09:03 -0500

    add responsive navbar

--- frontend/platoon-console/src/components/NavBar.jsx
+++ frontend/platoon-console/src/components/NavBar.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+import logo from '/src/assets/platoon-console-v2.svg'
 
-const NavBar = () => {
+const NavBar = ({ isLoggedIn, setIsLoggedIn }) => {
+
+const [collapsed, setCollapsed] = useState(true);
+// const toggleNavbar = () => setCollapsed(!collapsed);
+const [isOpen, setIsOpen] = useState(false);
+const navigate = useNavigate()
+
+
+const handleLogout = () => {
+  //delete token and update auth status
+  localStorage.removeItem('token')
+  setIsLoggedIn(false)
+  navigate('/login')
+}
+
+// const closeNavBar = () => {
+//   if (isOpen) {
+//     setIsOpen(false)
+//   }
+// };
+
+const toggleNavbar = () => {
+  setCollapsed(!collapsed)
+  setIsOpen(!isOpen)
+}
+
+
   return (
-    <div>NavBar</div>
+    <>
+      <Navbar className='navbar-expand-sm navbar-dark' >
+      <NavbarBrand href="/">
+        <img alt="logo" src={logo} style={{ height: 40, width: 40 }} />
+      </NavbarBrand>
+      <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+      <Collapse isOpen={isOpen} navbar>
+      <Nav className={!collapsed ? 'flex-column custom-navbar': ""}>
+        {isLoggedIn ? (
+          <>
+            <NavItem>
+              <NavLink active href="/" className='custom-link' >Home</NavLink>
+            </NavItem>  
+            <NavItem>
+              <NavLink active href="/login" className='custom-link'  onClick={() => { handleLogout(); toggleNavbar(); }}>Logout</NavLink>
+            </NavItem>  
+            <NavItem>
+              <NavLink active href="https://github.com/CodePlatoon" className='custom-link' >Github</NavLink>
+            </NavItem>  
+          </>
+        ) : (
+          <>
+            <NavItem>
+              <NavLink active href="/register" className='custom-link'>Register</NavLink>
+            </NavItem>  
+            <NavItem>
+              <NavLink active href="/login" className='custom-link'>Login</NavLink>
+            </NavItem>
+          </>
+        )}
+        </Nav>
+        </Collapse>
+      </Navbar>
+    </>
   )
 }
 
