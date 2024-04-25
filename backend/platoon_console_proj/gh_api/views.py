b01f6832598c8f8586c73b48620aec2cf785c378
Dee J. Mann <dee.j.mann@hotmail.com>
2024-04-25 11:23:22 -0400

    Added GhApiSerializer, GhApiConfigInfo Changes to be committed: 	new file:   serializers.py 	modified:   views.py

--- backend/platoon_console_proj/gh_api/views.py
+++ backend/platoon_console_proj/gh_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import GhApiConfigSerializer, GhApiConfig
 
 # Create your views here.
+
+class GhApiConfigInfo(APIView):
+    #TODO: Change this to instructor only, this is set to AllowAny just for testing purposes
+    permission_classes = [AllowAny]
+
+    def get(self, request, id):
+        # This method handles GET requests to display ghapi config values
+        ghapi_config = get_object_or_404(GhApiConfig, id=id)
+
+        ser_ghapi_config = GhApiConfigSerializer(ghapi_config)
+
+        return Response(ser_ghapi_config.data)
