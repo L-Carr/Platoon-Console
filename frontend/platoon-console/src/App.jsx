70f048ffd443173f5c8c6c1c27963f2d863030dc
dagaither <gaither@protonmail.com>
2024-04-26 17:22:28 -0400

    Updated user_app Login_user view and serializer to fix regression on backend.
    Fixed API call on Login.jsx and enhanced useEffect on App.jsx
    to redirect to login if no token in local storage.
    
     Changes to be committed:
    	modified:   ../../backend/platoon_console_proj/platoon_console_proj/settings.py
    	modified:   ../../backend/platoon_console_proj/user_app/migrations/0001_initial.py
    	modified:   ../../backend/platoon_console_proj/user_app/serializers.py
    	modified:   src/App.jsx
    	modified:   src/pages/Login.jsx

--- frontend/platoon-console/src/App.jsx
+++ frontend/platoon-console/src/App.jsx
@@ -1,32 +1,36 @@
 import { useState, useEffect } from 'react'
-import  { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import NavBar from './components/NavBar'
 
 const useLoggedIn = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  const useLoggedIn = () => {
-    useEffect(() => {
-      //check for auth with token in local storage
-      const isAuthenticated = localStorage.getItem('token') !== null
-      setIsLoggedIn(isAuthenticated)
-    }, [])
-  
-    return isLoggedIn
-  }
-}
+  useEffect(() => {
+    // Check for auth with token in local storage
+    const isAuthenticated = localStorage.getItem('token') !== null
+    setIsLoggedIn(isAuthenticated)
+  }, []) // Run once after the initial render
 
+  return isLoggedIn
+}
 
 function App() {
   const isLoggedIn = useLoggedIn()
+  const navigate = useNavigate()
   
+  useEffect(() => {
+    // Redirect to /login if not logged in
+    if (!isLoggedIn) {
+      navigate('/login')
+    }
+  }, [isLoggedIn, navigate]) // Re-run whenever isLoggedIn or navigate changes
 
   return (
     <>
-    <NavBar isLoggedIn={isLoggedIn} />
-    <div className="mainContent" style={{paddingTop: "8rem"}}>
-    <Outlet />
-    </div>
+      <NavBar isLoggedIn={isLoggedIn} />
+      <div className="mainContent" style={{paddingTop: "8rem"}}>
+        <Outlet />
+      </div>
     </>
   )
 }
