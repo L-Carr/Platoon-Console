280f061cb45d955c1c901b98cf6e440b25b98e68
Dee J. Mann <dee.j.mann@hotmail.com>
2024-04-29 20:13:40 -0400

    Added AllStudentDemoInfo Changes to be committed: 	modified:   backend/platoon_console_proj/demo/views.py

--- backend/platoon_console_proj/demo/views.py
+++ backend/platoon_console_proj/demo/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import DemoStudentSerializer, DemoStudent
 
 # Create your views here.
+
+class StudentDemoAllInfo(APIView):
+    #TODO: Change this to student and instructor only,  this is set to AllowAny just for testing purposes
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        student_demos = DemoStudent.objects.all()
+        ser_demos = DemoStudentSerializer(student_demos, many=True)
+
+        return Response(ser_demos.data)
