373f348403deb1acddc0424235566a4179e5e119
Dee J. Mann <dee.j.mann@hotmail.com>
2024-04-29 20:40:50 -0400

    Added endpoint /demo/all/<cohort_name/ Added view for all cohort demos Changes to be committed: 	modified:   backend/platoon_console_proj/demo/urls.py 	modified:   backend/platoon_console_proj/demo/views.py

--- backend/platoon_console_proj/demo/urls.py
+++ backend/platoon_console_proj/demo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AllStudentDemoInfo
+from .views import AllStudentDemoInfo, AllCohortDemoInfo
 
 urlpatterns = [
     path('all/', AllStudentDemoInfo.as_view(), name='get-all-demos'),
+    path('all/<str:cohort_name>/', AllCohortDemoInfo.as_view(), name='get-cohort-demos'),
 ]
--- backend/platoon_console_proj/demo/views.py
+++ backend/platoon_console_proj/demo/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from .serializers import DemoStudentSerializer, DemoStudent
+from cohort.models import Cohort
 
 # Create your views here.
 
@@ -12,7 +13,20 @@ class AllStudentDemoInfo(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
+        # This method handles GET requests to view all demo records
         student_demos = DemoStudent.objects.all()
         ser_demos = DemoStudentSerializer(student_demos, many=True)
 
+        return Response(ser_demos.data)
+    
+class AllCohortDemoInfo(APIView):
+    #TODO: Change this to student and instructor only, this is set to AllowAny just for testing purposes
+    permission_classes = [AllowAny]
+
+    def get(self, request, cohort_name):
+        # This method handles Get requests to view all demo records for a cohort
+        cohort = get_object_or_404(Cohort, cohort_name=cohort_name)
+        cohort_demos = DemoStudent.objects.filter(cohort=cohort)
+        ser_demos = DemoStudentSerializer(cohort_demos, many=True)
+        
         return Response(ser_demos.data)
