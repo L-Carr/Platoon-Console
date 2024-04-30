b223056acd4f2250cc468da58e62fd4d5a6dff9e
Dee J. Mann <dee.j.mann@hotmail.com>
2024-04-29 20:03:21 -0400

    Created basic DemoStudent model Changes to be committed: 	modified:   backend/platoon_console_proj/demo/models.py

--- backend/platoon_console_proj/demo/models.py
+++ backend/platoon_console_proj/demo/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from user_app.models import User
+from cohort.models import Cohort
 
 # Create your models here.
-class Demo(models.Model):
-    status = models.CharField(null=False, blank=False)
+class DemoStudent(models.Model):
+    status = models.CharField(max_length=10, null=False, blank=False, default='to do')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='demo_user')
+    cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE, related_name='demo_cohort')
+
+    def __str__(self):
+        return f'{self.user} {self.cohort}'
