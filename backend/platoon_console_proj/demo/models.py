e5f82ae631fb2bea859b0d902aa9a103b4f6b831
Dee J. Mann <dee.j.mann@hotmail.com>
2024-04-29 21:26:02 -0400

    Added validator to DemoStudent Model Changes to be committed: 	modified:   backend/platoon_console_proj/demo/models.py 	new file:   backend/platoon_console_proj/demo/validators.py

--- backend/platoon_console_proj/demo/models.py
+++ backend/platoon_console_proj/demo/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from user_app.models import UserAccount, User
 from cohort.models import Cohort
+from .validators import validate_status
 
 # Create your models here.
 class DemoStudent(models.Model):
-    status = models.CharField(max_length=10, null=False, blank=False, default='to do')
+    status = models.CharField(max_length=10, null=False, blank=False, default='to do', validators=[validate_status])
     student = models.OneToOneField(UserAccount, on_delete=models.CASCADE, related_name='demo_user')
     cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE, related_name='demo_cohort')
 
