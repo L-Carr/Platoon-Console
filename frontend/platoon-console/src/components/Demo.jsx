e6ccd2caabf5c37c67cbb4fb552f77f1feea228a
Dee J. Mann <dee.j.mann@hotmail.com>
2024-05-01 14:06:15 -0400

    Updated method for demos endpoint

--- frontend/platoon-console/src/components/Demo.jsx
+++ frontend/platoon-console/src/components/Demo.jsx
@@ -5,18 +5,19 @@ const Demo = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [demos, setDemos] = useState([])
 
-  const handleGetDemos = async (e) => {
+  const updateDemos = async (e) => {
+    // Uses a post request to check with backend and create records for any student who does not have a demo record, gets current updated list of all demo records
     try {
       const userCohort = "Whiskey"
 
-      let response = await axios.get(`https://127.0.0.1:8000/demo/all/${userCohort}/`, {
+      let response = await axios.post(`https://127.0.0.1:8000/demo/all/${userCohort}/`, {
         headers: {
           'Content-Type': 'application/json'
         }
       });
-      setDemos(response.data)
-      console.log('Get Demos')
+      console.log('Update Demos')
       console.log(await response.data)
+      setDemos(await response.data)
     } catch (error) {
       if (error.response) {
         const errorMessage = error.response.data;
@@ -27,8 +28,8 @@ const Demo = () => {
   }
 
   useEffect(() => {
-    handleGetDemos();
-  }, [])
+    updateDemos();
+  }, []);
 
 
   return (
