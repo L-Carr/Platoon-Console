1b759e1fa917ce0b463ae28cdd6b37b2de0331c3
Hank Caswell <david.h.caswell@proton.me>
2024-05-01 16:10:53 -0400

    added models for team and team membership

---
+++ backend/platoon_console_proj/teams/views.py
@@ -0,0 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Membership, Team
+from .serializers import MembershipSerializer, TeamSerializer
+
+class TeamMembershipList(APIView):
+    def get(self, request, team_id):
+        memberships = Membership.objects.filter(team_id=team_id)
+        serializer = MembershipSerializer(memberships, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class TeamListView(APIView):
+    def get(self, request):
+        teams = Team.objects.all()
+        serializer = TeamSerializer(teams, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
