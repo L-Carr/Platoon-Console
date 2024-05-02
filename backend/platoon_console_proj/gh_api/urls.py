01c049d1f8d9ba54e78450f8aa76dc07793dd521
Dee J. Mann <dee.j.mann@hotmail.com>
2024-05-02 12:38:14 -0400

    Updated url names Changes to be committed: 	modified:   backend/platoon_console_proj/gh_api/urls.py

--- backend/platoon_console_proj/gh_api/urls.py
+++ backend/platoon_console_proj/gh_api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', GhApiConfigCreate.as_view(), name='create-gh-config'),
     path('<int:id>/', GhApiConfigInfo.as_view(), name='get-gh-config'),
     path('all/', GhApiConfigViewAll.as_view(), name='all-gh-config'),
-    path('main/', GhApiMainRepo.as_view(), name='main-readme'),
-    path('week/<str:week>/', GhApiWeekDir.as_view(), name='week-readme'),
+    path('main/', GhApiMainRepo.as_view(), name='main-repo'),
+    path('week/<str:week>/', GhApiWeekDir.as_view(), name='week-dir'),
 ]
