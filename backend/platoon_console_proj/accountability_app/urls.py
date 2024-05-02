112b4a15876b1a99f67655e3d738f923c4b2b000
black-magk <landon.c.carr@gmail.com>
2024-05-01 18:20:37 -0600

    Added URL for override APIVIEW intended for instructors

--- backend/platoon_console_proj/accountability_app/urls.py
+++ backend/platoon_console_proj/accountability_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AttendanceRecordCreateOrUpdate,AttendanceRecordList # Import the register function
+from .views import AttendanceRecordCreateOrUpdate,AttendanceRecordList,AdminAttendanceOverride # Import the register function
 
 urlpatterns = [
      path('record/', AttendanceRecordCreateOrUpdate.as_view(), name='record-attendance'),
-     path('retrieve/', AttendanceRecordList.as_view(), name='retrieve-attendance')
+     path('retrieve/', AttendanceRecordList.as_view(), name='retrieve-attendance'),
+     path('alter/', AdminAttendanceOverride.as_view(), name='alter-attendance')
     # path('details/', AttendanceRecordDetails.as_view(), name='attendance-details')
     ]# Updated to use the register function]
   
--- backend/platoon_console_proj/accountability_app/views.py
+++ backend/platoon_console_proj/accountability_app/views.py
@@ -8,29 +8,29 @@ from .models import AttendanceRecord, User
 from .serializers import AttendanceRecordsSerializer
 
 
-
 class AttendanceRecordCreateOrUpdate(APIView):
     def post(self, request):
         # Get the Current Date
         today = timezone.localdate()
         # Get Current User from request
         user = request.user
-     
+
         # Get cohort name from request body
         cohort_name = request.data.get('cohort_name')
         # Return 404 if no cohort name is provided that matches current list of Cohorts
-        cohort = get_object_or_404(Cohort, cohort_name=cohort_name) 
+        cohort = get_object_or_404(Cohort, cohort_name=cohort_name)
 
         # Check for existing attendance records for the specified cohort and day
-        records_exist = AttendanceRecord.objects.filter(cohort=cohort, accountability_date=today).exists()
-        
-        #-------------------------------------------------------------------------------------------------------------------
+        records_exist = AttendanceRecord.objects.filter(
+            cohort=cohort, accountability_date=today).exists()
+
+        # -------------------------------------------------------------------------------------------------------------------
         # Does the record exist for the specified Cohort as a Whole? If not generate all records for a given cohort and date
         if not records_exist:
             # If no records exist, create them for all users in the cohort
-            #Return all users belonging to a cohort
+            # Return all users belonging to a cohort
             users = User.objects.filter(profile__cohort_name=cohort).distinct()
-            #Generate Data for all users in the cohort
+            # Generate Data for all users in the cohort
             for all_user in users:
                 print(f"all_user: {all_user.id}")
                 data = {
@@ -46,33 +46,37 @@ class AttendanceRecordCreateOrUpdate(APIView):
                 }
            # Serialize individual records for each user
                 # Serializer takes special context to be passed to serializer. Passing the user ( Not just user from request body)
-                serializer = AttendanceRecordsSerializer(data=data,context={'user': all_user})
+                serializer = AttendanceRecordsSerializer(
+                    data=data, context={'user': all_user})
                 if serializer.is_valid():
                     serializer.save()
                 else:
                     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-           
 
-            #Update the current user's record
-            record = AttendanceRecord.objects.get(user=user, accountability_date=today, cohort=cohort)
-            serializer = AttendanceRecordsSerializer(record, data=request.data, partial=True,context={'user': user})
+            # Update the current user's record
+            record = AttendanceRecord.objects.get(
+                user=user, accountability_date=today, cohort=cohort)
+            serializer = AttendanceRecordsSerializer(
+                record, data=request.data, partial=True, context={'user': user})
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        #-------------------------------------------------------------------------------------------------------------------
+        # -------------------------------------------------------------------------------------------------------------------
         # Edge Case. Attempt to get users record for the day even if cohort records already exist
-        record = AttendanceRecord.objects.get(user=user, accountability_date=today, cohort=cohort)
-        #if no record. Create one. Edge case, user signed up for original pull for some reason
+        record = AttendanceRecord.objects.get(
+            user=user, accountability_date=today, cohort=cohort)
+        # if no record. Create one. Edge case, user signed up for original pull for some reason
         if not record:
-            serializer = AttendanceRecordsSerializer(data=request.data,context={'user': request.user})
+            serializer = AttendanceRecordsSerializer(
+                data=request.data, context={'user': request.user})
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            
-        serializer = AttendanceRecordsSerializer(record, data=request.data, partial=True,context={'user': user})
+        serializer = AttendanceRecordsSerializer(
+            record, data=request.data, partial=True, context={'user': user})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -86,7 +90,8 @@ class AttendanceRecordList(APIView):
         if not cohort_name:
             return Response({"error": "Cohort name is required."}, status=status.HTTP_400_BAD_REQUEST)
 
-        cohort = get_object_or_404(Cohort, cohort_name=cohort_name)  # Safely get the Cohort instance by name
+        # Safely get the Cohort instance by name
+        cohort = get_object_or_404(Cohort, cohort_name=cohort_name)
 
         # Retrieve all records for this cohort
         records = AttendanceRecord.objects.filter(cohort=cohort)
@@ -97,14 +102,18 @@ class AttendanceRecordList(APIView):
             return Response({"message": "No attendance records found for this cohort."}, status=status.HTTP_404_NOT_FOUND)
 
 
-
 class AdminAttendanceOverride(APIView):
     """
     Updates an attendance record.
     """
+
     def patch(self, request, pk):
+        '''
+        Update method for Instructors to alter existing Attendance Records
+        '''
         attendance_record = get_object_or_404(AttendanceRecord, pk=pk)
-        serializer = AttendanceRecordsSerializer(attendance_record, data=request.data, partial=True)
+        serializer = AttendanceRecordsSerializer(
+            attendance_record, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
