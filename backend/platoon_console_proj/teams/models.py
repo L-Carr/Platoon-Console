f439bc6a412ead6e65d98f50df1c24b815d1edb7
Hank Caswell <david.h.caswell@proton.me>
2024-05-05 10:16:21 -0400

    added cohort model on team

--- backend/platoon_console_proj/teams/models.py
+++ backend/platoon_console_proj/teams/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cohort.models import Cohort
 
 # Create your models here.
 from django.db import models
@@ -7,6 +8,9 @@ from django.db import models
 class Team(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE, related_name='teams')
+    
+    
 
     def __str__(self):
         return self.name
