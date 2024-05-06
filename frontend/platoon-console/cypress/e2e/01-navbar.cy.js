b35ce718aab7c5920505465c58a5d9981b229aa6
Neka Blair <ourblairfamily@gmail.com>
2024-05-06 08:18:35 -0500

    cypress testing in progress

---
+++ frontend/platoon-console/cypress/e2e/01-navbar.cy.js
@@ -0,0 +1,42 @@
+describe('Navbar', () => {
+    context('when logged out', () => {
+      beforeEach(() => {
+        // Visit the page with the navbar component while simulating that the user is logged out
+        cy.visit('http://localhost:5173/', { isLoggedIn: false });
+      });
+  
+      it('displays only login and register links', () => {
+        // Check that the "Register" link is visible and has the correct href attribute
+        cy.get('nav').contains('Register').should('be.visible')
+        // .and('have.attr', 'href', '/register');
+  
+        // Check that the "Login" link is visible and has the correct href attribute
+        cy.get('nav').contains('Login').should('be.visible')
+        // .and('have.attr', 'href', '/login');
+  
+        // Ensure that other links are not visible
+        cy.get('nav').contains('Home').should('not.exist');
+        cy.get('nav').contains('Logout').should('not.exist');
+      });
+    });
+
+    context('when logged in', () => {
+        beforeEach(() => {
+          // Visit the page with the navbar component while simulating that the user is logged in
+          cy.visit('http://localhost:5173/', { isLoggedIn: true });
+        });
+    
+        it('displays home and logout links', () => {
+          // Check that the "Home" link is visible and has the correct href attribute
+          cy.get('nav').contains('Home').should('be.visible').and('have.attr', 'href', '/');
+    
+          // Check that the "Logout" link is visible and has the correct href attribute
+          cy.get('nav').contains('Logout').should('be.visible').and('have.attr', 'href', '/login');
+    
+          // Ensure that other links are not visible
+          cy.get('nav').contains('Register').should('not.exist');
+          cy.get('nav').contains('Login').should('not.exist');
+        });
+      });
+    });
+  
---
+++ frontend/platoon-console/cypress/e2e/03-login.cy.js
@@ -0,0 +1,31 @@
+describe('User Login', () => {
+    beforeEach(() => {
+        //visit login page before each test
+        cy.visit('http://localhost:5173/login')
+    })
+
+    it('successfully logs in a user with valid credentials', () => {
+        //fill out login form and grab and apply tokens from localStorage
+        cy.get("exampleEmail").type('john.doe@example.com')
+        cy.get('#examplePassword').type('password')
+
+        //check token
+        Cypress.Commands.add('checkToken', (token) => {
+            cy.window().its('localStorage.token').should('eq', token)
+        })
+        cy.checkToken('abc123')
+
+
+        //submit form
+        cy.get('form').submit()
+
+        //send post request to login endpoint
+        cy.request({
+            url: 'http://localhost:8000/user/login/',
+            method: 'POST',
+            body: {
+                email:
+            }
+        })
+    })
+})
