9a2b21202fba4f55159b793ee09be383d8c98547
black-magk <landon.c.carr@gmail.com>
2024-05-06 08:49:58 -0600

    Error Handling

--- backend/platoon_console_proj/user_app/views.py
+++ backend/platoon_console_proj/user_app/views.py
@@ -15,7 +15,7 @@ from rest_framework.authentication import TokenAuthentication
 from django.contrib.auth.password_validation import validate_password
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
-
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
 from user_app.permissions import IsInstructor, IsStudent,IsAttendanceRecords
@@ -68,6 +68,8 @@ class UserRegistration(APIView):
             if token:
                 # Send a welcome email to the new user.
                 single_email_distro(user.email, "Platoon.Console Registration", "Welcome to Platoon.Console,", "Thank you for registering with Platoon.Console. We hope you enjoy Code Platoon as much as we did.")
+            
+            print('User Registered')
             return Response({'message': 'User registered.'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -81,6 +83,9 @@ class UserPasswordReset(APIView):
         """
         email = request.data.get('email')  # Extract the email address from the request data.
         user = get_user_model()
+
+        # if not user:
+        #     return Response({'error': 'Invalid email'}, status=status.HTTP_400_BAD_REQUEST)
         try:
             user = user.objects.get(email=email)  # Find the user by their email.
             token = default_token_generator.make_token(user)  # Generate a password reset token.
@@ -92,10 +97,11 @@ class UserPasswordReset(APIView):
             # Send the password reset email.
             link2 = f"http://localhost:5173/change-password/{uidb64}/{token}/"
             single_email_distro(email, "Platoon.Console Password Reset", "You've reached Platoon.Console Support,", f"You have requested a password reset. Please click the link below to reset your password. If you did not request this, please ignore this email. {link2}")
-            return Response({'message': 'Password reset email sent.'}, status=status.HTTP_200_OK)
+            return Response('Password reset email sent.', status=status.HTTP_200_OK)
         except user.DoesNotExist:
-            return Response({'error': 'User does not exist.'}, status=status.HTTP_404_NOT_FOUND)
-
+            print('User Does not Exist')
+            return Response('User Does Not Exist', status=status.HTTP_404_NOT_FOUND)
+            
     def put(self, request, uidb64, token):
         """
         Resets the user's password if the provided token is valid.
@@ -104,11 +110,17 @@ class UserPasswordReset(APIView):
             uid = force_str(urlsafe_base64_decode(uidb64))  # Decode the user's ID from base64.
             user = get_user_model().objects.get(pk=uid)  # Retrieve the user by decoded ID.
         except (TypeError, ValueError, OverflowError, user.DoesNotExist):
-            return Response({'error': 'Invalid link'}, status=status.HTTP_400_BAD_REQUEST)
+            print('Invalid Link')
+            return Response('Invalid link', status=status.HTTP_400_BAD_REQUEST)
 
         if user is not None and default_token_generator.check_token(user, token):
             new_password = request.data.get('new_password')
-            validate_password(new_password, user)
+            try:
+                validate_password(new_password, user)
+            except ValidationError as e:
+                print(f'Password Does not meet the validation Error: {e}')
+                return Response(f'Password Does not meet the validation Error: {e}' , status=status.HTTP_400_BAD_REQUEST)
+
             user.set_password(new_password)  # Set and hash the new password
             user.save()  # Get the new password from request data.
             # user.set_password(new_password)  # Set and hash the new password.
