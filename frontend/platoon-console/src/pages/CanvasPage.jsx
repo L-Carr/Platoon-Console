355092622e7099bcbad9f6cb8f8c4153f4eb79d0
Neka Blair <ourblairfamily@gmail.com>
2024-05-07 15:28:16 -0500

    able to save as screenshot as png to computer

--- frontend/platoon-console/src/pages/CanvasPage.jsx
+++ frontend/platoon-console/src/pages/CanvasPage.jsx
@@ -1,6 +1,9 @@
 import React, { useLayoutEffect, useState, useRef } from 'react'
 import rough from 'roughjs'
 import './CanvasPage.css'
+import { useNavigate } from 'react-router-dom';
+import html2canvas from 'html2canvas';
+
 
 const generator = rough.generator()
 
@@ -9,11 +12,12 @@ function createElement(x1, y1, x2, y2, type) {
     return { x1, y1, x2, y2, roughElement}
 }
 
-
 const CanvasPage = () => {
     const [elements, setElements] = useState([])
     const [drawing, setDrawing] = useState(false)
     const [elementType, setElementType] = useState("line")
+
+    const navigate = useNavigate();
 
 
     //perform layout measurements before browser repaints screen
@@ -44,36 +48,55 @@ const CanvasPage = () => {
         const elementsCopy = [...elements]
         elementsCopy[index] = updatedElement
         setElements(elementsCopy)
-
     }
 
     const handleMouseUp = () => {
         setDrawing(false)
     }
 
+    const handleGoHome = () => {
+        navigate('/');
+    };
+
+    const handleSave = () => {
+        const canvas = document.getElementById("canvas");
+        const context = canvas.getContext("2d");
+        // Take a screenshot of the entire page
+        html2canvas(document.body).then(canvas => {
+        // Convert the screenshot to a data URL
+        const imgData = canvas.toDataURL('image/png');
+        const link = document.createElement('a');
+        link.download = 'drawing.png';
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+        });
+    }
+
   return (
     <>
     <div>
-        <div style={{ position: "fixed" }}>
+        <div style={{ position: "fixed", zIndex: 2 }}>
             <input 
                 type="radio" 
                 id="line" 
                 checked={elementType === "line"} 
                 onChange={() => setElementType("line")} />
-            <label htmlFor="line">Line</label>
+            <label htmlFor="line" style={{color: "black"}}>Line</label>
             <input
                 type="radio"
                 id="rectangle"
                 checked={elementType === "rectangle"}
                 onChange={() => setElementType("rectangle")}
             />
-            <label htmlFor="rectangle">Rectangle</label>
+            <label htmlFor="rectangle" style={{color: "black"}}>Rectangle</label>
+            <button onClick={handleGoHome}>Home</button>
+            <button onClick={handleSave}>Save</button>
         </div>
         <canvas 
         id="canvas" 
-        style={{backgroundColor: '#fefae0'}} 
-        height={window.innerHeight-50} 
-        width={window.innerWidth-200}
+        style={{backgroundColor: '#ffffff', position: 'fixed', top: 0, left: 0, zIndex: -1}} 
+        height={window.innerHeight} 
+        width={window.innerWidth}
         onMouseDown = {handleMouseDown}
         onMouseMove = {handleMouseMove}
         onMouseUp = {handleMouseUp}
