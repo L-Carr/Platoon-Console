6d8ccdcc7b503d8e250f7bd07da47aef59ba4d9a
Neka Blair <ourblairfamily@gmail.com>
2024-05-07 11:14:22 -0500

    cleaning up comments and additional unused code

--- frontend/platoon-console/cypress/e2e/04-homepage.cy.js
+++ frontend/platoon-console/cypress/e2e/04-homepage.cy.js
@@ -17,55 +17,42 @@ describe('User Login and Homepage Links', () => {
 
   it('toggles accountability modal when "Attendance Check In" link is clicked', () => {
       cy.contains('Attendance Check In').click()
-      // .should('contain', 'x')
-      // .click()
-      // cy.get('[data-test="accountability-modal"]').should('be.visible');
   });
 
   it('toggles daily feedback modal when "Daily Feedback Form" link is clicked', () => {
       cy.contains('Daily Feedback Form').click();
-      // cy.get('[data-test="feedback-modal"]').should('be.visible');
   });
 
   it('toggles daily feedback modal when "Daily Agenda" link is clicked', () => {
     cy.contains('Daily Agenda').click();
-    // cy.get('[data-test="feedback-modal"]').should('be.visible');
     });
 
   it('toggles daily feedback modal when "Monthly Schedule" link is clicked', () => {
   cy.contains('Monthly Schedule').click();
-  // cy.get('[data-test="feedback-modal"]').should('be.visible');
   });
 
   it('toggles daily feedback modal when "Curriculum" link is clicked', () => {
     cy.contains('Curriculum').click();
-    // cy.get('[data-test="feedback-modal"]').should('be.visible');
   });
 
   it('toggles daily feedback modal when "Demos & Notes" link is clicked', () => {
     cy.contains('Demos & Notes').click();
-    // cy.get('[data-test="feedback-modal"]').should('be.visible');
   });
 
   it('toggles daily feedback modal when "Assignments" link is clicked', () => {
     cy.contains('Assignments').click();
-    // cy.get('[data-test="feedback-modal"]').should('be.visible');
 });
 
   it('toggles daily feedback modal when "Generate Pairs" link is clicked', () => {
     cy.contains('Generate Pairs').click();
-    // cy.get('[data-test="feedback-modal"]').should('be.visible');
 });
 
   it('toggles daily feedback modal when "Generate Demo List" link is clicked', () => {
     cy.contains('Generate Demo List').click();
-    // cy.get('[data-test="feedback-modal"]').should('be.visible');
   });
 
   it('toggles daily feedback modal when "YouTube Playlist" link is clicked', () => {
     cy.contains('YouTube Playlist').click();
-    // cy.get('[data-test="feedback-modal"]').should('be.visible');
   });
-  
   
 });
