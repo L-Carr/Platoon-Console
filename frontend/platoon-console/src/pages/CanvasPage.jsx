d5d21ec9736f8fc48648bd1bd1d5c19f4c1d7422
Neka Blair <ourblairfamily@gmail.com>
2024-05-07 13:43:41 -0500

    add logic to get mouse x and y coordinates, and to grab coordinates of lines we draw from beginning to end

--- frontend/platoon-console/src/pages/CanvasPage.jsx
+++ frontend/platoon-console/src/pages/CanvasPage.jsx
@@ -3,8 +3,13 @@ import rough from 'roughjs'
 
 const generator = rough.generator()
 
+function createElement(x1, y1, x2, y2) {
+    const roughElement = generator.line(x1, y1, x2, y2)
+    return { x1, y1, x2, y2, roughElement}
+}
+
 const CanvasPage = () => {
-    const [element, setElement] = useState([])
+    const [elements, setElements] = useState([])
     const [drawing, setDrawing] = useState(false)
 
 
@@ -26,13 +31,25 @@ const CanvasPage = () => {
     
     const handleMouseDown = (event) => {
         setDrawing(true)
+        const {clientX, clientY} = event
+        const element = createElement(clientX, clientY, clientX, clientY)
+        //add each element as
+        setElements(prevState => [...prevState, element])
     }
 
     const handleMouseMove = (event) => {
         if(!drawing) return
         //clientX and clientY are the mouse movements
         const {clientX, clientY} = event
+        const index = elements.length - 1
+        const {x1, y1} = elements[index]
+        const updatedElement = createElement(x1, y1, clientX, clientY)
         console.log(clientX, clientY)
+
+        const elementsCopy = [...elements]
+        elementsCopy[index] = updatedElement
+        setElements(elementsCopy)
+
     }
 
     const handleMouseUp = () => {
