9ebdaa402fac0a45182431d6daf2219cf08a9cc0
Neka Blair <ourblairfamily@gmail.com>
2024-05-07 14:44:53 -0500

    able to make lines and rectangles, but cursor is offset, need to fix

--- frontend/platoon-console/src/pages/CanvasPage.jsx
+++ frontend/platoon-console/src/pages/CanvasPage.jsx
@@ -1,19 +1,19 @@
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useLayoutEffect, useState, useRef } from 'react'
 import rough from 'roughjs'
 import './CanvasPage.css'
 
 const generator = rough.generator()
 
-function createElement(x1, y1, x2, y2) {
-    const roughElement = generator.line(x1, y1, x2, y2)
+function createElement(x1, y1, x2, y2, type) {
+    const roughElement = type === "line" ? generator.line(x1, y1, x2, y2) : generator.rectangle(x1, y1, x2 - x1, y2 - y1, {roughness: 0})
     return { x1, y1, x2, y2, roughElement}
 }
+
 
 const CanvasPage = () => {
     const [elements, setElements] = useState([])
     const [drawing, setDrawing] = useState(false)
-
-
+    const [elementType, setElementType] = useState("line")
 
 
     //perform layout measurements before browser repaints screen
@@ -22,30 +22,25 @@ const CanvasPage = () => {
         const context = canvas.getContext("2d")
         context.clearRect(0, 0, canvas.width, canvas.height)
         const roughCanvas = rough.canvas(canvas)
-
         elements.forEach(element => roughCanvas.draw(element.roughElement))
 
-
-        // roughCanvas.draw(line)
     }, [elements])
     
     const handleMouseDown = (event) => {
         setDrawing(true)
         const {clientX, clientY} = event
-        const element = createElement(clientX, clientY, clientX, clientY)
-        //add each element as
-        setElements(prevState => [...prevState, element])
+        const element = createElement(clientX, clientY, clientX, clientY, elementType)
+        //add each element into array
+        setElements((prevState) => [...prevState, element])
     }
 
     const handleMouseMove = (event) => {
         if(!drawing) return
-        //clientX and clientY are the mouse movements
         const {clientX, clientY} = event
         const index = elements.length - 1
         const {x1, y1} = elements[index]
-        const updatedElement = createElement(x1, y1, clientX, clientY)
+        const updatedElement = createElement(x1, y1, clientX, clientY, elementType)
         console.log(clientX, clientY)
-
         const elementsCopy = [...elements]
         elementsCopy[index] = updatedElement
         setElements(elementsCopy)
@@ -58,20 +53,34 @@ const CanvasPage = () => {
 
   return (
     <>
-    <div>CanvasPage</div>
+    <div>
+        <div style={{ position: "fixed" }}>
+            <input 
+                type="radio" 
+                id="line" 
+                checked={elementType === "line"} 
+                onChange={() => setElementType("line")} />
+            <label htmlFor="line">Line</label>
+            <input
+                type="radio"
+                id="rectangle"
+                checked={elementType === "rectangle"}
+                onChange={() => setElementType("rectangle")}
+            />
+            <label htmlFor="rectangle">Rectangle</label>
+        </div>
         <canvas 
         id="canvas" 
         style={{backgroundColor: '#fefae0'}} 
-        height={window.innerHeight} 
-        width={window.innerWidth}
+        height={window.innerHeight-50} 
+        width={window.innerWidth-200}
         onMouseDown = {handleMouseDown}
         onMouseMove = {handleMouseMove}
         onMouseUp = {handleMouseUp}
-        
-        
         >
         Canvas
         </canvas>
+    </div>
     </>
   )
 }
